918606f3553f7443d98082403c01e4fea44911cb
Mies Dela Cruz <mieseliel.delacruz@gmail.com>
2023-02-01 23:04:19 +0800

    #3 - Update reddit script and creds

--- reddit_script.py
+++ reddit_script.py
@@ -1,3 +1,24 @@
-reddit_secret = open("reddit_secret.txt")
-lines = reddit_secret.readlines()
-print(lines[0])
+import requests
+#fetch creds
+with open("reddit_secret.txt", 'r') as read:
+  lines = read.read()
+
+
+CLIENT_ID: str = lines[0]
+
+CLIENT_SECRET: str = lines[1]
+
+print(CLIENT_SECRET)
+
+#get access token from reddit
+auth = requests.auth.HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
+
+#setup credentials
+data: dict[str, str] = {
+  'grant_type': 'password',
+  'username': '',
+  'password': '',
+}
+
+headers = {'User-Agent': 'Test API v0.0.1'}
+res = requests.post('', auth=auth, data=data,headers=headers)
