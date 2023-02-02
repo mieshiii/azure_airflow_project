78c9ba3e9a92e22c712d9f11b30d79580d1ae800
Mies Dela Cruz <mieseliel.delacruz@gmail.com>
2023-02-02 23:53:40 +0800

    #4 - Add account creds and update gitignore

--- reddit_script.py
+++ reddit_script.py
@@ -1,14 +1,18 @@
 import requests
 #fetch creds
-with open("reddit_secret.txt", 'r') as read:
-  lines = read.read()
+with open("reddit_secret.txt", "r") as read:
+  creds_lines = read.read()
 
 
-CLIENT_ID: str = lines[0]
+CLIENT_ID: str = creds_lines[0]
 
-CLIENT_SECRET: str = lines[1]
+CLIENT_SECRET: str = creds_lines[1]
 
-print(CLIENT_SECRET)
+with open("reddit_account.txt", "r") as read:
+  account_lines = read.read()
+  
+reddit_username: str = account_lines[0]
+reddit_password: str = account_lines[1]
 
 #get access token from reddit
 auth = requests.auth.HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
@@ -16,9 +20,10 @@ auth = requests.auth.HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
 #setup credentials
 data: dict[str, str] = {
   'grant_type': 'password',
-  'username': '',
-  'password': '',
+  'username': reddit_username,
+  'password': reddit_password,
 }
 
+print(reddit_username)
 headers = {'User-Agent': 'Test API v0.0.1'}
 res = requests.post('', auth=auth, data=data,headers=headers)
