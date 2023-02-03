9a717a524dd3b136b9cca40c6da037d16b4f5f5f
Mies Dela Cruz <mieseliel.delacruz@gmail.com>
2023-02-03 23:52:39 +0800

    #5 - fixed readlines

--- reddit_script.py
+++ reddit_script.py
@@ -1,15 +1,16 @@
 import requests
-#fetch creds
-with open("reddit_secret.txt", "r") as read:
-  creds_lines = read.read()
+#fetch api creds
+with open("reddit_secret.txt", "r") as creds:
+  creds_lines = creds.readlines()
 
 
 CLIENT_ID: str = creds_lines[0]
 
 CLIENT_SECRET: str = creds_lines[1]
 
-with open("reddit_account.txt", "r") as read:
-  account_lines = read.read()
+#fetch reddit account creds
+with open("reddit_account.txt", "r") as account:
+  account_lines = account.readlines()
   
 reddit_username: str = account_lines[0]
 reddit_password: str = account_lines[1]
@@ -19,11 +20,10 @@ auth = requests.auth.HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
 
 #setup credentials
 data: dict[str, str] = {
-  'grant_type': 'password',
-  'username': reddit_username,
-  'password': reddit_password,
-}
+   'grant_type': 'password',
+   'username': reddit_username,
+   'password': reddit_password,
+ }
 
-print(reddit_username)
 headers = {'User-Agent': 'Test API v0.0.1'}
 res = requests.post('', auth=auth, data=data,headers=headers)
