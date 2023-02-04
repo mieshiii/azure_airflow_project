3f88415fc8c92a19f9977e4e2b7e63b2732a0d0e
Mies Dela Cruz <mieseliel.delacruz@gmail.com>
2023-02-04 23:54:31 +0800

    #6 - finalize scraping script

--- reddit_script.py
+++ reddit_script.py
@@ -4,16 +4,16 @@ with open("reddit_secret.txt", "r") as creds:
   creds_lines = creds.readlines()
 
 
-CLIENT_ID: str = creds_lines[0]
+CLIENT_ID: str = creds_lines[0].rstrip()
 
-CLIENT_SECRET: str = creds_lines[1]
+CLIENT_SECRET: str = creds_lines[1].rstrip()
 
 #fetch reddit account creds
 with open("reddit_account.txt", "r") as account:
-  account_lines = account.readlines()
+  account_lines: list[str] = account.readlines()
   
-reddit_username: str = account_lines[0]
-reddit_password: str = account_lines[1]
+reddit_username: str = account_lines[0].rstrip()
+reddit_password: str = account_lines[1].rstrip()
 
 #get access token from reddit
 auth = requests.auth.HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
@@ -25,5 +25,20 @@ data: dict[str, str] = {
    'password': reddit_password,
  }
 
-headers = {'User-Agent': 'Test API v0.0.1'}
-res = requests.post('', auth=auth, data=data,headers=headers)
+headers = {'User-Agent':  reddit_username + '_Reddit_API/v0.0.1'}
+#token request
+res = requests.post('https://www.reddit.com/api/v1/access_token', 
+                    auth=auth, 
+                    data=data, 
+                    headers=headers)
+
+#token
+reddit_token = res.json()['access_token']
+
+#append token to headers
+headers['Authorization'] = f'bearer {reddit_token}'
+
+#get most popular post from a subreddit
+reddit = requests.get('https://oauth.reddit.com/r/wallstreetbets/hot', headers=headers).json()
+
+print(reddit)
